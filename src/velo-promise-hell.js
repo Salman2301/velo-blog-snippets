faac9d7311e13bff1e47b76093c98cbf3a3a3048
Salman K.A.A <40920317+Salman2301@users.noreply.github.com>
2023-10-01 23:08:56 +0200

    feat: add velo promise hell snippets

---
+++ src/velo-promise-hell.js
@@ -0,0 +1,27 @@
+import wixData from "wix-data";
+import wixUsers from "wix-users";
+
+let currentUserEmail;
+
+$w.onReady(()=>{
+    if( wixUsers.currentUser.loggedIn ) {
+        wixUsers.promptLogin({ mode: "login"})
+        .then(user=>{
+            return user.getEmail()
+        })
+        .then(email=>{
+            currentUserEmail = email;
+            return wixData.query("myUser")
+            .eq("email", email)
+            .find()
+        })
+        .then(resUser=>{
+            if( resUser.length > 0 ) {
+                console.log("User logged in and stored user email in the 'myUser' database")
+            }
+            else {
+                return wixData.insert("myUser", { email: currentUserEmail })
+            }
+        })
+    }
+});
